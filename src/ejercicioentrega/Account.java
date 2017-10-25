92da0f0a151eb0505589c72e506eb424bced4d50
Angelica <Angelica@Marucha>
2017-10-24 22:42:58 -0500

    Inten to

---
+++ src/ejercicioentrega/Account.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioentrega;
+
+
+public class Account {
+    private int id;
+    private double credit;
+
+    public Account(int id, double credit) {
+        this.id = id;
+        this.credit = credit;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public void setCredit(double credit) {
+        this.credit = credit;
+    }
+    
+    
+
+}
---
+++ src/ejercicioentrega/Client.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioentrega;
+
+public class Client extends Person {
+    
+    private Manager manager;
+
+    public Client(String name) {
+        super(name);
+        this.manager=manager;
+    }
+    
+    
+    
+}
