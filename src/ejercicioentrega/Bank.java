29723d5e427a16e6e3925b3be9156aa51b1a9178
Cristian Chitiva <cychitivav@unal.edu.co>
2017-10-25 22:19:21 -0500

    Merge origin/master
    Conflicts:
    	src/ejercicioentrega/Bank.java
    	src/ejercicioentrega/Client.java

--- src/ejercicioentrega/Bank.java
+++ src/ejercicioentrega/Bank.java
@@ -23,7 +23,6 @@ public class Bank {
 
     public String getName() {
         return name;
-
     }
 
     public void setName(String name) {
@@ -32,28 +31,35 @@ public class Bank {
 
     //Nueva Cuenta
     public boolean addAccount(int id, double credit, Client dueno) {
-        Account account = new Account(0, 0, dueno);
+        Account account = null;
+        try {
+            account = new Account(id, credit, dueno);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
         return this.accounts.add(account);
     }
 
     //Genera Manager
     public boolean newManager(String name) {
-        Manager manager = new Manager(name);
-        if (this.managers.add(manager)) {
-            return true;
-        } else {
-            return false;
+        Manager manager = null;
+        try {
+            manager = new Manager(name);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
+        return this.managers.add(manager);
     }
 
-//Genera Cliente
+    //Genera Cliente
     public boolean newClient(String name) {
-        Client client = new Client(name);
-        if (this.clients.add(client)) {
-            return true;
-        } else {
-            return false;
+        Client client = null;
+        try {
+            client = new Client(name);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
+        return this.clients.add(client);
     }
 
     @Override
--- src/ejercicioentrega/Client.java
+++ src/ejercicioentrega/Client.java
@@ -17,21 +17,21 @@ public class Client extends Person {
         this.manager = manager;
         this.accounts = new ArrayList<>();
     }
-   
+
     //Nueva Cuenta
-    public boolean addAccount(int id, double credit, Client dueno){
-        Account account = new Account(0, 0.0 , null);
-        if(this.accounts.add(account))
-            return true;
-        else
-            return false;
+    public boolean addAccount(Account account) {
+        Account newAccount = null;
+        try {
+            newAccount = account;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return this.accounts.add(newAccount);
     }
 
     @Override
     public String toString() {
-        return "Cliente:    " + super.toString() + "|| Manager=" + manager + "|| Cuentas=" + accounts.size() ;
+        return "Cliente:    " + super.toString() + "|| Manager=" + manager + "|| Cuentas=" + accounts.size();
     }
-    
-    
-    
+
 }
