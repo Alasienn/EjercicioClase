bf3bae39de82dd31aaf14434e5efef352e4f8c6e
Alassien <Alassien@Mancera-Alfa>
2017-10-25 19:37:38 -0500

    Merge origin/master

--- src/ejercicioentrega/Manager.java
+++ src/ejercicioentrega/Manager.java
@@ -5,10 +5,24 @@
  */
 package ejercicioentrega;
 
+import java.util.ArrayList;
+
 public class Manager extends Person {
+    
+    private ArrayList<Client> clients;
     
     public Manager(String name) {
         super(name);
+        this.clients = new ArrayList<>();
+    }
+    
+    
+    public boolean newClient(String name){
+        Client client = new Client(name);
+        if(this.clients.add(client))
+            return true;
+        else
+            return false;
     }
     
 }
